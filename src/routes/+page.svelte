716ef1bf80c38c88d6eb65f18457863b92bab008
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-03-16 00:56:47 +0800

    Implement conversion thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script>
 	let colors = ['bg-yellow-700', 'bg-purple-800', 'bg-blue-800', 'bg-teal-700'];
+	//colors = ['bg-blue-800', 'bg-blue-800', 'bg-blue-800', 'bg-blue-800'];
+	let ratio = [
+		[1, 3],
+		[1, 3],
+		[1, 3],
+		[1, 3]
+	];
+	let have = [6, 6, 6, 6];
+
+	function convert(id, from) {
+		have[id] = Number(have[id]) + from * ratio[id][0];
+		have[id + 1] = Number(have[id + 1]) + from * -ratio[id][1];
+	}
 </script>
 
 <svelte:head>
@@ -9,21 +22,25 @@
 
 <section class="p-4">
 	<div class="flex gap-4 items-center">
+		<button class="text-white bg-black px-2 py-1">R</button>
 		{#each colors as color, i}
-			<div class="bg-gray-300 rounded-lg overflow-hidden">
+			<div class="bg-gray-200 rounded-lg overflow-hidden">
 				<div class="size-24 {colors[i]} rounded-br-2xl"></div>
 				<input
 					type="text"
-					class="bg-transparent text-center text-lg py-1 w-full"
+					class="bg-transparent text-center text-lg py-1 rounded-b-lg w-full {have[i] < 0
+						? 'text-red-700'
+						: ''}"
 					size="1"
-					value="3"
+					bind:value={have[i]}
 				/>
 			</div>
 			{#if i < colors.length - 1}
-				<div class="flex flex-col">
-					<button class="{colors[i]} text-white">⩤</button>
-					<input type="text" size="1" value="1:3" class="text-center" />
-					<button class="{colors[i + 1]} text-white">⩥</button>
+				<div class="flex flex-col rounded-md overflow-hidden">
+					<button class="{colors[i]} text-white" on:click={() => convert(i, 1)}>⩤</button>
+					<!--TODO: bind <input> to ratio[][]-->
+					<input type="text" size="1" value="1:3" class="text-center bg-gray-200" />
+					<button class="{colors[i + 1]} text-white" on:click={() => convert(i, -1)}>⩥</button>
 				</div>
 			{/if}
 		{/each}
